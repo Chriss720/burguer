cfd06d5e1cba1f2797d74a12d60b4a43f6fdc386
Cristian Mercado Martin <cristianmercado720@gmail.com>
2025-11-03 17:17:20 -0600

    Componentes desde la bd

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,15 @@
 /* eslint-disable prettier/prettier */
 import { Controller, Get, Render } from '@nestjs/common';
+import { ProductService } from './product/product.service';
 
 @Controller()
 export class AppController {
+  constructor(private readonly productService: ProductService) {}
   @Get()
   @Render('index')
-  root() {
-    return {};
+  async root() {
+    const products = await this.productService.findAvailable();
+    return { products };
   }
 
   @Get('acceder')
